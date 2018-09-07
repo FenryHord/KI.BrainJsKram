9dfbac95e8b1bf086ffaedf5b7bd55bef0af3436
Fenry Hord <maik.w4@web.de>
2018-09-07 09:29:16 +0200

    cleanData

--- TrumpVsKim/TrumpKim.js
+++ TrumpVsKim/TrumpKim.js
@@ -16,37 +16,51 @@ let kimTestData = JSON.parse(
   fs.readFileSync('./Rohdaten/kardashianTestData.json')
 );
 
-const generateTestData = (trumpData, KimData) => {
+const generateTestData = (dataArr, namesArr) => {
   let trainDat = [];
-  trumpData.forEach(element => {
-    trainDat.push({
-      input: bow(element, voc),
-      output: { Trump: 1 }
+  dataArr.forEach((element, index) => {
+    element.forEach(element => {
+      trainDat.push({
+        input: bow(element, voc),
+        output: { [namesArr[index]]: 1 }
+      });
     });
   });
-  KimData.forEach(element => {
-    trainDat.push({
-      input: bow(element, voc),
-      output: { Kim: 1 }
-    });
-  });
-
   return trainDat;
 };
 
-const allRawTrainData = trumpTrainData.concat(kimTrainData, trumpTestData, kimTestData);
+const cleanData = data => {
+  data.map((element, index) => {
+    if (element.indexOf('"@') >= 0) {
+      data.splice(index, 1);
+    }
+  });
+};
+
+cleanData(trumpTrainData);
+cleanData(trumpTestData);
+cleanData(kimTrainData);
+cleanData(kimTestData);
+
+const allRawTrainData = trumpTrainData.concat(
+  kimTrainData,
+  trumpTestData,
+  kimTestData
+);
 const voc = dict(allRawTrainData);
-const generatedTrianigData = generateTestData(trumpTrainData, kimTrainData);
+const generatedTrianigData = generateTestData(
+  [trumpTrainData, kimTrainData],
+  ['Trump', 'Kim']
+);
 
-
-const net = new brain.NeuralNetwork({ hiddenLayers: [20] });
+const net = new brain.NeuralNetwork({ hiddenLayers: [10, 10] });
 net.setActivation('sigmoid');
 const MAXITERATIONS = 100;
 const conf = {
   iterations: MAXITERATIONS,
   log: true,
   logPeriod: 10,
-  callback: () => {
+  /* callback: () => {
     for (var i = generatedTrianigData.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
       var temp = generatedTrianigData[i];
@@ -54,36 +68,43 @@ const conf = {
       generatedTrianigData[j] = temp;
     }
   },
-  callbackPerid: 10,
+  callbackPerid: 10,*/
   //learningRate: 0.3,
   errorThresh: 0.000005
 };
-net.train(generatedTrianigData, conf);
 
-let testResult = "";
-let fromTrump = true;
-for (let i = 0; i < 20; i++) {
-  if (Math.random() < 0.5) {
-    fromTrump = true;
-    testResult = trumpTestData[Math.round(Math.random() * trumpTestData.length)];
-    console.log("Trump", testResult.trimRight());
+
+const trainAndSave = () => {
+  net.train(generatedTrianigData, conf);
+  let lastJSON = net.toJSON();
+  fs.writeFileSync('./JSON/lastJSON.json', JSON.stringify(lastJSON));
+};
+
+const testTraining = () => {
+  let testResult = '';
+  let fromTrump = true;
+  for (let i = 0; i < 20; i++) {
+    if (Math.random() < 0.5) {
+      fromTrump = true;
+      testResult =
+        trumpTestData[Math.round(Math.random() * trumpTestData.length)];
+      console.log('Trump', testResult.trimRight());
+    } else {
+      fromTrump = false;
+      testResult = kimTestData[Math.round(Math.random() * kimTestData.length)];
+      console.log('Kim', testResult.trimRight());
+    }
+    let result = net.run(bow(testResult, voc));
+    console.log('Result: ', result);
+    if (fromTrump === true && result.Trump > result.Kim) {
+      console.log('RICHTIG es war TRUMP');
+    } else if (fromTrump === false && result.Trump < result.Kim) {
+      console.log('RICHTIG es war KIM');
+    } else {
+      console.log('FALSCH');
+    }
+    console.log('----------------------------------------');
   }
-  else {
-    fromTrump = false;
-    testResult = kimTestData[Math.round(Math.random() * kimTestData.length)];
-    console.log("Kim", testResult.trimRight());
-  }
-  let result = net.run(bow(testResult, voc));
-  console.log("Result: ", result);
-  if (fromTrump === true && result.Trump > result.Kim) {
-    console.log("RICHTIG es war TRUMP");
-  }
-  else if (fromTrump === false && result.Trump < result.Kim) {
-    console.log("RICHTIG es war KIM");
-  }
-  else {
-    console.log("FALSCH");
-  }
-  console.log("----------------------------------------");
-}
-//console.log(bow("congratulations", voc));
+};
+trainAndSave();
+testTraining();
